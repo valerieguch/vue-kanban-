45a8c69a78a22397e5c8e2fb6aeef162fda27827
Valerieguch <valeria.guch2000@gmail.com>
2022-11-29 04:42:51 +0300

    Move the header to a separate component

--- src/AppKanban.vue
+++ src/AppKanban.vue
@@ -1,15 +1,10 @@
 <script setup>
-import IconEcosystem from './components/icons/IconEcosystem.vue';
+import Header from './components/Header.vue'
 import Column from './components/Column.vue'
 import ModalItemCreate from './components/ModalItemCreate.vue'
 
 import { ref } from 'vue'
 import { kanban, archived } from './store.js'
-
-// TODO maybe move this to a header component?
-const preferDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-if (preferDarkTheme) document.querySelector('body').classList.add('dark');
-let isDarkTheme = ref(preferDarkTheme)
 
 function switchTheme() {
   isDarkTheme = !isDarkTheme
@@ -21,28 +16,7 @@ const showModal = ref(false)
 
 
 <template>
-  <header class="header">
-    <div class="header-wrapper">
-      <a href="#" class="header-logo">
-        <IconEcosystem />
-      </a>
-
-      <!-- TODO make centered, probably using grid -->
-      <h1 class="header-app-name">Канбан</h1>
-
-      <div class="theme-switch-wrapper">
-        <label for="switch">Темная тема</label>
-        <input
-          type="checkbox"
-          :checked="preferDarkTheme"
-          @change="switchTheme"
-          id="switch"
-          class="theme-switch">
-      </div>
-
-      <!-- <button @click="switchTheme">Switch theme</button> -->
-    </div>
-  </header>
+  <Header />
 
   <main class="main-wrapper">
     <button @click="showModal = true" class="btn-create-task">Создать задачу</button>
@@ -84,35 +58,6 @@ const showModal = ref(false)
 
 
 <style scoped>
-.header {
-  background-color: var(--color-background-mute);
-}
-
-.header-wrapper {
-  min-height: 3rem;
-  max-width: 80rem;
-  margin: 0 auto;
-  padding: 0 2rem;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.header-logo {
-  color: var(--color-text);
-  padding: 0.2rem;
-  scale: 150%;
-}
-
-.header-app-name {
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-
-  font-size: 1.5em;
-}
-
 .main-wrapper {
   margin: 0 auto;
   display: flex;
@@ -157,32 +102,6 @@ const showModal = ref(false)
 /*    grid-template-columns: unset;*/
 /*    grid-template-rows: unset;*/
 /*    overflow: scroll;*/
-}
-
-.theme-switch-wrapper {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-
-.theme-switch {
-  border-radius: 2em;
-  width: 40px;
-  height: 20px;
-  background-color: #fff;
-  border: 1px solid var(--color-border);
-  appearance: none;
-  cursor: pointer;
-  background-repeat: no-repeat;
-  background-position: 0;
-  background-image: url('./assets/img/switch.svg');
-  transition: background-position .2s ease-in-out;
-}
-
-.theme-switch:checked {
-  background-position: right center;
-  background-image: url('./assets/img/switch-checked.svg');
-  background-color: #776acf;
 }
 
 .spacer {
---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<script setup>
+import IconEcosystem from './icons/IconEcosystem.vue';
+
+// If the user prefers dark theme, then use the colors from `body.dark`
+const preferDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+
+if (preferDarkTheme) {
+  document.querySelector('body').classList.add('dark');
+}
+
+function switchTheme() {
+  document.querySelector('body').classList.toggle('dark');
+}
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <a href="#" class="header-logo">
+        <IconEcosystem />
+      </a>
+
+      <!-- TODO make centered, probably using grid -->
+      <h1 class="header-app-name">Канбан</h1>
+
+      <div class="theme-switch-wrapper">
+        <label for="switch">Темная тема</label>
+        <input
+          type="checkbox"
+          :checked="preferDarkTheme"
+          @change="switchTheme"
+          id="switch"
+          class="theme-switch">
+      </div>
+
+      <!-- <button @click="switchTheme">Switch theme</button> -->
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: var(--color-background-mute);
+}
+
+.header-wrapper {
+  min-height: 3rem;
+  max-width: 80rem;
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-logo {
+  color: var(--color-text);
+  padding: 0.2rem;
+  scale: 150%;
+}
+
+.header-app-name {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  font-size: 1.5em;
+}
+
+.theme-switch-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.theme-switch {
+  border-radius: 2em;
+  width: 40px;
+  height: 20px;
+  background-color: #fff;
+  border: 1px solid var(--color-border);
+  appearance: none;
+  cursor: pointer;
+  background-repeat: no-repeat;
+  background-position: 0;
+  background-image: url('../assets/img/switch.svg');
+  transition: background-position .2s ease-in-out;
+}
+
+.theme-switch:checked {
+  background-position: right center;
+  background-image: url('../assets/img/switch-checked.svg');
+  background-color: #776acf;
+}
+</style>
